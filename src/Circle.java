bb4edc98070e753d54a58b1966898d83cfd7960f
Simon Nöjd <simon.nojd.jobb@gmail.com>
2020-10-21 14:36:27 +0200

    Added 3 files

---
+++ src/Circle.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Simon Nöjd
+ * Date: 2020-10-21
+ * Time: 14:34
+ * Project: Test
+ * Copyright: MIT
+ */
+public class Circle {
+    public static void main(String[] args) {
+        int radius = 2;
+    }
+}
---
+++ src/Triangle.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Simon Nöjd
+ * Date: 2020-10-21
+ * Time: 14:33
+ * Project: Test
+ * Copyright: MIT
+ */
+public class Triangle {
+    public static void main(String[] args) {
+        int height = 5;
+    }
+}
